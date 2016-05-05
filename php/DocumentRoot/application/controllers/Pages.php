3cc698e1006ce62b50fa69c7543585316c167b56
Ending <357628953@qq.com>
2016-05-06 02:05:08 +0800

    routine push

--- php/DocumentRoot/application/controllers/Pages.php
+++ php/DocumentRoot/application/controllers/Pages.php
@@ -1,6 +1,13 @@
 <?php
     class Pages extends CI_Controller {
         public function view( $page = 'home' ) {
+            if( !file_exists( APPPATH.'views/pages/'.$page.'.php' ) ) {
+                show_404( ) ;
+            }
+            $data['title'] = ucfirst( $page ) ;
+            $this->load->view( 'templates/header' , $data ) ;
+            $this->load->view( 'pages/'.$page , $data ) ;
+            $this->load->view( 'templates/footer' , $data ) ;
         }
     }
 ?>
---
+++ php/DocumentRoot/application/views/templates/footer.php
@@ -0,0 +1,3 @@
+        <em>&copy; 2016</em>
+    </body>
+</html>
