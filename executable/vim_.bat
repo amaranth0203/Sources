e3ad079a98312b42c2f4ce707b6d7a00fb4f31e0
qiyunhu <qiyunhu@bbktel.com>
2016-05-06 09:07:24 +0800

    routine push

--- executable/vim_.bat
+++ executable/vim_.bat
@@ -1,54 +1,21 @@
 @echo off
-rem -- Run Vim --
+setlocal enabledelayedexpansion
+if "%identity%" == "Shadow" (
+    set "VIM_EXE_DIR=D:\Program Files\Vim\vim74\"
+)
+if "%identity%" == "vivo_work" (
+    set "VIM_EXE_DIR=D:\Program Files (x86)\Vim\vim74\"
+)
+
+if "%VIM_EXE_DIR%"=="" (
+    echo cannot find VIM_EXE_DIR
+    pause
+    goto :eof
+)
+
 if exist "%UserProfile%/qyh_session_vim.vim" (
-    "D:\Program Files\Vim\vim74\vim.exe" -u "C:\Users\Ending\.qyh_vim\vimrc" -S "%UserProfile%/qyh_session_vim.vim" %*
+    "%VIM_EXE_DIR%/vim.exe" -u %UserProfile%/.qyh_vim/vimrc -S "%UserProfile%/qyh_session_vim.vim"  %*
 ) else (
     echo. 2> %UserProfile%/qyh_session_vim.vim
-    "D:\Program Files\Vim\vim74\vim.exe" -u "C:\Users\Ending\.qyh_vim\vimrc" -S "%UserProfile%/qyh_session_vim.vim" %*
+    "%VIM_EXE_DIR%/vim.exe" -u %UserProfile%/.qyh_vim/vimrc -S "%UserProfile%/qyh_session_vim.vim"  %*
 )
-goto eof
-
-set VIM_EXE_DIR=D:\Program Files\Vim\vim74\
-if exist "%VIM%\vim74\vim.exe" set VIM_EXE_DIR=%VIM%\vim74
-if exist "%VIMRUNTIME%\vim.exe" set VIM_EXE_DIR=%VIMRUNTIME%
-
-if exist "%VIM_EXE_DIR%\vim.exe" goto havevim
-echo "%VIM_EXE_DIR%\vim.exe" not found
-goto eof
-
-:havevim
-rem collect the arguments in VIMARGS for Win95
-set VIMARGS=
-set VIMNOFORK=
-:loopstart
-if .%1==. goto loopend
-if NOT .%1==.-f goto noforkarg
-set VIMNOFORK=1
-:noforkarg
-set VIMARGS=%VIMARGS% %1
-shift
-goto loopstart
-:loopend
-
-if .%OS%==.Windows_NT goto ntaction
-
-if .%VIMNOFORK%==.1 goto nofork
-start "%VIM_EXE_DIR%\vim.exe" -u C:\Users\Ending\.qyh_vim\vimrc  %VIMARGS%
-goto eof
-
-:nofork
-start /w "%VIM_EXE_DIR%\vim.exe" -u C:\Users\Ending\.qyh_vim\vimrc  %VIMARGS%
-goto eof
-
-:ntaction
-rem for WinNT we can use %*
-if .%VIMNOFORK%==.1 goto noforknt
-start "dummy" /b "%VIM_EXE_DIR%\vim.exe" -u C:\Users\Ending\.qyh_vim\vimrc  %*
-goto eof
-
-:noforknt
-start "dummy" /b /wait "%VIM_EXE_DIR%\vim.exe" -u C:\Users\Ending\.qyh_vim\vimrc  %*
-
-:eof
-set VIMARGS=
-set VIMNOFORK=
