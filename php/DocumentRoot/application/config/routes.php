d810fc4aacbed458e6c1cfb4795f6912f326c3d8
Ending <357628953@qq.com>
2016-05-08 23:13:29 +0800

    routine push

--- php/DocumentRoot/application/config/routes.php
+++ php/DocumentRoot/application/config/routes.php
@@ -57,4 +57,5 @@ $route['news/create'] = 'news/create' ;
 $route['news/(:any)'] = 'news/view/$1' ;
 $route['news'] = 'news' ;
 $route['(:any)'] = 'pages/view/$1' ;
-$route['default_controller'] = 'pages/view';
+//$route['default_controller'] = 'pages/view';
+$route['default_controller'] = 'news';
