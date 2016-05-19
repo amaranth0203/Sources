45afc09845d9045059c577e52413117d43e0d8f6
qiyunhu <qiyunhu@bbktel.com>
2016-05-19 14:36:16 +0800

    routine push

--- executable/qyh.py
+++ executable/qyh.py
@@ -15,7 +15,7 @@ class qyh_base( object ) :
         print repo.git.status( )
         print repo.git.add( '.' )
         print repo.git.commit( m = "routine push" )
-        origin = repo.remotes.origin
+        origin = repo.remote.origin
         ssh_executable = os.path.join( "" , 'my_ssh_executable.sh' )
         repo.git.update_environment( GIT_SSH_COMMAND = ssh_executable )
         origin.push( repo.active_branch.name )
