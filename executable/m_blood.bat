57514a77b18526367b4e21f8ea30e5d3cc0ed84d
qiyunhu <845687884@qq.com>
2016-06-06 14:46:04 +0000

    routine push

--- executable/m_blood.bat
+++ executable/m_blood.bat
@@ -1,2 +1,11 @@
-@set PATH=D:\cygwin64\bin;%PATH%
-@call "D:\cygwin64\bin\mintty.exe" -c "d:/cygwin64/home/administrator/.minttyrc_blood" 
+@echo off
+setlocal enabledelayedexpansion
+
+if "%identity%" == "Shadow" (
+    @set !PATH!=D:\cygwin64\bin;!PATH!
+    @call "D:\cygwin64\bin\mintty.exe" -c "d:/cygwin64/home/Ending/.minttyrc_blood" 
+)
+if "%identity%" == "vivo_work" (
+    @set !PATH!=D:\cygwin64\bin;!PATH!
+    @call "D:\cygwin64\bin\mintty.exe" -c "d:/cygwin64/home/administrator/.minttyrc_blood" 
+)
