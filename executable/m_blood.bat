868452b82a932668dd28db7a612d0b1b279cccb1
qiyunhu <qiyunhu@bbktel.com>
2016-06-06 16:12:08 +0800

    routine push

---
+++ executable/m_blood.bat
@@ -0,0 +1,2 @@
+@set PATH=D:\cygwin64\bin;%PATH%
+@call "D:\cygwin64\bin\mintty.exe" -c "d:/cygwin64/home/administrator/.minttyrc_blood" 
--- executable/mintty.bat
+++
@@ -1,2 +0,0 @@
-@set PATH=D:\cygwin64\bin;%PATH%
-@call "D:\cygwin64\bin\mintty.exe"
