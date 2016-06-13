ab9087f544ed2e3caed343f5e8f2952c038ea8e5
qiyunhu <qiyunhu@bbktel.com>
2016-06-13 21:48:39 +0800

    inherit identity to cygwin

--- executable/m_blood.bat
+++ executable/m_blood.bat
@@ -7,5 +7,8 @@ if "%identity%" == "Shadow" (
 )
 if "%identity%" == "vivo_work" (
     @set PATH=D:\cygwin64\bin;!PATH!
+    @set tmp_identity=!identity!
+    @set identity=
+    @set identity=!tmp_identity!
     @call "D:\cygwin64\bin\mintty.exe" -c "d:/cygwin64/home/administrator/.minttyrc_blood" 
 )
