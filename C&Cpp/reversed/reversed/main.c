44e7d725d6108e956e7581b7d87358b1052ca4b4
qiyunhu <qiyunhu@bbktel.com>
2016-11-01 18:00:06 +0800

    routine push

---
+++ C&Cpp/reversed/reversed/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <windows.h>
+#include "funcs.h"
+
+int main(int argc, char *argv[])
+{
+  MessageBox( NULL , "aaaa" , "bbbb" , MB_OK ) ;
+  return 0;
+}
+
