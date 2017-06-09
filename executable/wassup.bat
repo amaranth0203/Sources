5197570a07cd8792b20fa6cc517f37f8c7097b4d
qiyunhu <845687884@qq.com>
2017-06-09 08:47:45 +0800

    update wassup path for shadow

--- executable/wassup.bat
+++ executable/wassup.bat
@@ -1,24 +1,26 @@
 @echo off
-@set PATH=%PATH%;D:\.wassup\sqlmap
-@set PATH=%PATH%;D:\.wassup\Nmap
-@set PATH=%PATH%;D:\.wassup
-@set PATH=%PATH%;d:\binary
-@set PATH=%PATH%;d:\binary\MinGW64\bin
-@set PATH=%PATH%;d:\binary\Ruby21-x64\bin
-@set PATH=%PATH%;d:\binary\MinGW\bin
-@set PATH=%PATH%;d:\binary\OpenSSH-Win64
-@set PATH=%PATH%;d:\putty
-@set PATH=%PATH%;d:\Program Files\nodejs
-@set PATH=%PATH%;d:\Program Files\php-5.6.21-Win32-VC11-x64
-@set PATH=%PATH%;d:\Program Files\Apache24\bin
-@set PATH=%PATH%;d:\Program Files\tidy-5.2.0-win64\bin
-@set PATH=%PATH%;d:\Program Files\MySQL\MySQL Server 5.0\bin
-@set PATH=%PATH%;d:\Program Files (x86)\CMake\bin
-@set PATH=%PATH%;d:\Program Files (x86)\Imatest\toolbox\matlab
-@set PATH=%PATH%;d:\Program Files (x86)\Imatest\bin\win32
-@set PATH=%PATH%;d:\Program Files (x86)\Imatest
-@set PATH=%PATH%;c:\Users\Administrator.AXX3DX8ZNMSGU5B\AppData\Roaming\npm
-@set PATH=%PATH%;c:\Program Files (x86)\NASM
+@set BINARY_HOME=D:\binary
+@set PYTHON_HOME=D:\binary\Python34_32
+@set PYQT_HOME=D:\binary\python34_32\Lib\site-packages\PyQt4
+@set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_111
+REM D:\Program Files\Java\jdk1.6.0_45
+REM D:\Program Files\Java\jdk1.7.0_67
+@set NMAP_HOME=D:\Program Files\nmap-6.49BETA2-win32
+@set SVN_HOME=D:\Program Files\Subversion\bin
+@set NODE_HOME=D:\Program Files\nodejs\
+@set FIREFOX_HOME=D:\Program Files\Mozilla Firefox
+@set MINGW_HOME=D:\binary\mingw32
+@set TIDY_HOME=D:\Program Files\tidy-5.2.0-win64\bin
+@set PHP_HOME=D:\Program Files\php-5.6.21-Win32-VC11-x64
+@set MYSQL_HOME=D:\Program Files\MySQL\MySQL Server 5.0\bin
+@set APACHE_HOME=D:\Program Files\Apache24
+@set SSH_HOME=D:\Program Files\OpenSSH-Win64
+@set GLOBAL_HOME=D:\binary\glo656wb\bin
+@set NC_HOME=D:\binary\netcat-1.11
+
+@set ws=E:\Source
+@set wn=E:\Stay\txt_and_more\notes
+@set PATH=%PATH%;%BINARY_HOME%;%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%PYQT_HOME%;%JAVA_HOME%\bin;%NMAP_HOME%;%SVN_HOME%;%NODE_HOME%;%FIREFOX_HOME%;%MINGW_HOME%\bin;%TIDY_HOME%;%PHP_HOME%;%MYSQL_HOME%;%APACHE_HOME%\bin;%SSH_HOME%;%GLOBAL_HOME%;%NC_HOME%;
 @set HTTP_PROXY=localhost:2333
 @set HTTPS_PROXY=localhost:2333
 
